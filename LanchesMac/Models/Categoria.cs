ac8c1f2e2aa3c92c244dd90b17f5f27700d28387
Guilherme Anselmi <60523666+GuilhermeAnselmi@users.noreply.github.com>
2022-01-08 21:08:02 -0300

    Adicionar arquivos de projeto.

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models;
+
+[Table("Categorias")]
+public class Categoria
+{
+    [Key]
+    public int CategoriaId { get; set; }
+    [Required(ErrorMessage = "Nome da categoria é exigida")]
+    [StringLength(100, ErrorMessage = "Tamanho maximo é 100")]
+    [Display(Name = "Nome")]
+    public string Nome { get; set; }
+    [StringLength(200, ErrorMessage = "Tamanho maximo é 200")]
+    [Required(ErrorMessage = "Descrição da categoria é exigida")]
+    public string Descricao { get; set; }
+
+    public List<Lanche> Lanches { get; set; }
+}
---
+++ LanchesMac/Models/Lanche.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models;
+
+[Table("Lanches")]
+public class Lanche
+{
+    [Key]
+    public int LancheId { get; set; }
+    [Required(ErrorMessage = "Erro no nome do lanche"), Display(Name = "Nome do lanche")]
+    public string Nome { get; set; }
+    public string DescricaoCurta { get; set; }
+    public string DescricaoDetalhada { get; set; }
+    public decimal Preco { get; set; }
+    public string ImagemUrl { get; set; }
+    public string ImagemThumbnailUrl { get; set; }
+    public bool LanchePreferido { get; set; }
+    public bool Estoque { get; set; }
+
+    public int CategoriaId { get; set; }
+    public virtual Categoria Categoria { get; set; }
+}
